c5cd1fc0c73b03977a0ec4eb5dfcfc5d11454031
sergey <loboff09@gmail.com>
2024-05-06 13:16:46 +0300

    end

--- t2.py
+++ t2.py
@@ -13,20 +13,35 @@ def common_divs(data: list) -> list:
     if len(data) <= 0:
         return None
     else:
+        divs = []
         array = quicksort(data)
-        great = array[0]
-        for i in range(1, len(array)):
-            divs = []
-            num_f = great
-            num_s = array[i]
-            while num_f != 1:
-                mod = num_f % num_s
-                if mod == 0:
-                    divs.append(num_s)
-                num_f = num_s
-                num_s = mod
-            print(divs)
+        less = array[-1]
+        if less < 0:
+            less -= less * 2
+        for i in range(1, less + 1):
+            c = 0
+            for el in array:
+                if el % i != 0:
+                    break
+                c += 1
+            if c == len(array):
+                divs.append(i)
+        if len(divs) == 0:
+            return None
+        return divs
 
 
-test = [42, 12, 18]
-print(common_divs(test))
+test_1 = [42, 12, 18]
+print(f"test_1 - {common_divs(test_1)}")
+test_2 = [0, 0, 0]
+print(f"test_2 - {common_divs(test_2)}")
+test_3 = [42, 12, 18, -2]
+print(f"test_3 - {common_divs(test_3)}")
+test_4 = [42, 12, 18, 13, 13]
+print(f"test_4 - {common_divs(test_4)}")
+test_5 = []
+print(f"test_5 - {common_divs(test_5)}")
+test_6 = [42]
+print(f"test_6 - {common_divs(test_6)}")
+test_7 = [42, 12, 18, 0]
+print(f"test_7 - {common_divs(test_7)}")
--- t3.py
+++ t3.py
@@ -1,1 +1,33 @@
+from typing import Optional
 
+
+def simple_num(ls: int, gt: int) -> list[Optional[int]]:
+    if ls > gt:
+        ls, gt = gt, ls
+    simple_list = []
+    for num in range(ls, gt + 1):
+        i = 1
+        half = num / 2
+        counter = 0
+        if num == 1:
+            simple_list.append(num)
+        else:
+            while i <= half and counter < 2:
+                if num % i == 0:
+                    counter += 1
+                i += 1
+            if counter == 1:
+                simple_list.append(num)
+    return simple_list
+
+
+test_1 = (12, 27)
+print(f"test_1 - {simple_num(*test_1)}")
+test_2 = (0, 0)
+print(f"test_2 - {simple_num(*test_2)}")
+test_3 = (-3, 3)
+print(f"test_3 - {simple_num(*test_3)}")
+test_4 = (1, 2)
+print(f"test_4 - {simple_num(*test_4)}")
+test_5 = (27, 12)
+print(f"test_5 - {simple_num(*test_5)}")
